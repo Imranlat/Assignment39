48926c44cce43d9e6b4fd7116b09deec9be7fd2d
Imran latif <159417772+Imranlat@users.noreply.github.com>
2024-04-03 14:01:30 +0500

    Add files via upload

---
+++ q39.js
@@ -0,0 +1,8 @@
+"use strict";
+//39.  Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+function city_country(city, country) {
+    return `${city}, ${country}`;
+}
+console.log(city_country("Lahore", "Pakistan"));
+console.log(city_country("maldiv", "singapore"));
+console.log(city_country("malashya", "turkey"));
